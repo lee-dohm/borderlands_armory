e097319e4d35c09bee01bbaea5e59f377ae117ab
Lee Dohm <lee@liftedstudios.com>
2012-09-26 19:30:11 -0700

    Initialize Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BorderlandsArmory::Application.config.secret_token = '4714dc8f049324cdd8d9f1dd315ac1e208932b5c0f62a2558b4dbd2bdb2e5c6693d1ffbbb6dc08956662329ec07ce734e6454de35b2722f544d78a9479c087a2'
